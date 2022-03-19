48162306b1ce94b566538d191e7e90724a6ddd7b
PeterRojkovski <84549374+PeterRojkovski@users.noreply.github.com>
2022-03-18 21:33:55 -0600

    Add files via upload

---
+++ Projects/Project 1/script.js
@@ -0,0 +1,10 @@
+let a='What\'s your favourite '
+let b="colour"
+let c=a+b+'?'
+alert(c);
+a=37
+b=70
+c=a*b
+alert(c)
+const amISatisfiedInLife=false
+//Excited and happy, I'm glad to be able to have a hand-on experience with code, and I can't wait to learn html and CSS
